175b7386f815959d9a2caf78c85648dbb11ea8f7
suicidekillerx <104750154+suicidekillerx@users.noreply.github.com>
2023-12-14 23:27:15 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdbrMhMc0jwgx_SB-sY8cgo11yorMuoCs",
+  authDomain: "test78-d2c4f.firebaseapp.com",
+  databaseURL: "https://test78-d2c4f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test78-d2c4f",
+  storageBucket: "test78-d2c4f.appspot.com",
+  messagingSenderId: "713296888254",
+  appId: "1:713296888254:web:7be768c6b81044589cb9ef",
+  measurementId: "G-MEHESY01H3"
+};
